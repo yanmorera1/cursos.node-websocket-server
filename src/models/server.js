c212d251c00e0c619464dde5410f6fd191778c87
YanMorera <90201379+YanMorera@users.noreply.github.com>
2023-07-14 10:40:03 -0500

    update: folders

--- src/models/server.js
+++ src/models/server.js
@@ -2,45 +2,45 @@ import express from 'express'
 import cors from 'cors'
 import { createServer } from 'http'
 import { Server as io } from 'socket.io'
-import { socketController } from '../../sockets/controller.js'
+import { socketController } from '../sockets/controller.js'
 
 export default class Server {
-    constructor() {
-        this.app = express()
-        this.port = process.env.PORT
-        this.server = createServer(this.app)
-        this.io = new io(this.server)
+  constructor() {
+    this.app = express()
+    this.port = process.env.PORT
+    this.server = createServer(this.app)
+    this.io = new io(this.server)
 
-        this.paths = {}
+    this.paths = {}
 
-        //Middlewares
-        this.middlewares()
-        //Routes
-        this.routes()
-        //Sockets
-        this.sockets()
-    }
+    //Middlewares
+    this.middlewares()
+    //Routes
+    this.routes()
+    //Sockets
+    this.sockets()
+  }
 
-    middlewares() {
-        //CORS
-        this.app.use(cors())
-        //read and parse body
-        this.app.use(express.json())
-        //public folder
-        this.app.use(express.static('src/public'))
-    }
+  middlewares() {
+    //CORS
+    this.app.use(cors())
+    //read and parse body
+    this.app.use(express.json())
+    //public folder
+    this.app.use(express.static('src/public'))
+  }
 
-    routes() {
-        // this.app.use(this.paths.users, userRouter)
-    }
+  routes() {
+    // this.app.use(this.paths.users, userRouter)
+  }
 
-    sockets() {
-        this.io.on('connection', socketController)
-    }
+  sockets() {
+    this.io.on('connection', socketController)
+  }
 
-    listen() {
-        this.server.listen(this.port, () => {
-            console.log(`Example websocket app listening on port ${this.port}`)
-        })
-    }
+  listen() {
+    this.server.listen(this.port, () => {
+      console.log(`Example websocket app listening on port ${this.port}`)
+    })
+  }
 }
